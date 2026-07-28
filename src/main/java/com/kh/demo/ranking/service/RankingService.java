2867f6c2a9a81f2aa4737b9c80b30cce95383d01
wngyrnr <wngyrmr@gmail.com>
2026-07-28 16:08:53 +0900

    css 수정

--- src/main/java/com/kh/demo/ranking/service/RankingService.java
+++ src/main/java/com/kh/demo/ranking/service/RankingService.java
@@ -3,6 +3,7 @@ package com.kh.demo.ranking.service;
 import com.kh.demo.ranking.dto.RankingDto;
 import java.util.List;
 
+
 public interface RankingService {
     List<RankingDto> getRankingBoard();
 }
